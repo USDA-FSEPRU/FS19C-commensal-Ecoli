06ae462ac2aebd9a1a1f001a779a7c0456688e15
k39ajdM2 <kathy.mou@ars.usda.gov>
2021-04-12 20:45:17 -0500

    update with search script

---
+++ scripts/search.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Beginning of file" > pa.isolates.txt
+for line in `cat Ecolivirulencegene.txt`; 
+do
+	echo $line >> pa.isolates.txt
+	grep -n "$line" gene_presence_absence_2.txt >> pa.isolates.txt
+	echo "END_OF_GENE" >> pa.isolates.txt
+
+done	
+echo "End of file" >> pa.isolates.txt
+
+
+# to be able to branch and whatnot, would have to do something like this
+# for x in file.txt:
+# 	gene = x;
+# 	output = grep gene in O157h7.txt
+# 	n = count number of ":"
+# 	line numbers = regex{\d+:}
+# 	if n==0:
+#		print(gene, "not found")
+# 	else:
+#		print("found",n,"times on lines:", output)
